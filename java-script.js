e03ca0a35fb25ed56a31207528aec620ebde4837
omar <omar.s.alshoul@gmail.com>
2019-09-24 16:36:26 +0300

    lord

---
+++ java-script.js
@@ -0,0 +1,21 @@
+JavaScript is the programming language of HTML and the Web.
+
+JavaScript is easy to learn.
+
+This tutorial will teach you JavaScript from basic to advanced.
+
+Why Study JavaScript?
+1- HTML to define the content of web pages
+2. CSS to specify the layout of web pages
+3. JavaScript to program the behavior of web pages
+
+
+In HTML, JavaScript code must be inserted between <script> and </script> tags.
+
+for example :
+
+<script>
+document.getElementById("demo").innerHTML = "My First JavaScript";
+</script>
+
+Scripts can be placed in the <body>, or in the <head> section of an HTML page, or in both.
